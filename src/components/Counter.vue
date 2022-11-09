5fe368fef188f8bc251c0c19dfb1e994c9076d17
Philip Mark Gutierrez <pipzgutz@gmail.com>
2022-11-09 10:13:43 +0800

    installed primeflex, primevue and primeicons

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="card">
-    <div class="count">{{ $store.state.count }}</div>
-    <div class="buttons">
-      <button class="btn mr-1" @click="$store.dispatch('decreaseCount')">
-        -
-      </button>
-      <button class="btn" @click="$store.dispatch('increaseCount')">+</button>
+  <div class="flex justify-content-center align-items-center flex-column">
+    <div class="text-8xl pt-4">{{ $store.state.count }}</div>
+    <div>
+      <Button label="-" @click="$store.dispatch('decreaseCount')" class="text-4xl p-4 mr-2"/>
+
+      <Button label="+" @click="$store.dispatch('increaseCount')" class="text-4xl p-4"/>
     </div>
   </div>
 </template>
@@ -22,25 +21,4 @@ export default {
 </script>
 
 <style scoped>
-.card {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-}
-
-.count {
-  font-size: 5em;
-  padding-top: 2em;
-}
-
-.btn {
-  font-size: 2em;
-  padding-left: 1em;
-  padding-right: 1em;
-}
-
-.mr-1 {
-  margin-right: 1em;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,17 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import PrimeVue from 'primevue/config'
+import Button from 'primevue/button'
+
 import './style.css'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
+app.use(PrimeVue, {ripple: false})
+
+app.component('Button', Button)
 
 app.mount('#app')
